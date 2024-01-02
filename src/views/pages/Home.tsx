985c164c933fc62eb136b7948e64d1780c9e326b
Gabriel Mandelli <gabriel-mandelli@hotmail.com>
2024-01-02 15:59:46 -0300

    Release

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -5,10 +5,10 @@ import backgroundImage from '../../assets/background.png'
 function Home() {
   return (
     <div className="home">
-    <div className="overlay">
-      <h1>Bem-vindos ao Nosso Casamento</h1>
-      <p>Descubra o amor que nos une</p>
-    </div>
+      <div className="overlay">
+        <h1>Bem-vindos ao Nosso Casamento</h1>
+        <p>Descubra o amor que nos une</p>
+      </div>
   </div>
   );
 }
